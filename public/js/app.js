be733b12261928bec2a75cfcae45d2168fe01777
Brian Hulette <hulettbh@gmail.com>
2015-08-19 08:08:21 -0700

    updated front-end to actually use contact list API

--- public/js/app.js
+++ public/js/app.js
@@ -22,17 +22,18 @@
       if (id === undefined)
         return $http.get('./api/contacts');
       else
-        return $http.get('./api/contacts/?id=' + id);
+        return $http.get('./api/contacts/' + id);
     };
     return result;
   }]);
 
   app.controller('ContactListController', ['$scope', 'Contacts', function($scope, Contacts){
     console.log('Creating ContactListController');
+    Contacts.get().success(function(data) { $scope.contacts = data; });
   }]);
   app.controller('ContactController', ['$scope', '$routeParams', 'Contacts', function($scope, $routeParams, Contacts){
     console.log('Creating ContactController');
-    $scope.contact_id = $routeParams.contact_id;
+    Contacts.get($routeParams.contact_id).success(function(data) { $scope.contact = data; });
     console.log($scope.contact_id);
   }]);
 })();
