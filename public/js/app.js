8cd5bf5c4450ef861ee8cab4b613a019eb563be4
Brian Hulette <hulettbh@gmail.com>
2015-08-21 13:13:54 -0700

    added directive for modeling phone number list

--- public/js/app.js
+++ public/js/app.js
@@ -54,10 +54,7 @@
     $scope.contact_id = $routeParams.contact_id;
     //Contacts.get($scope.contact_id).success(function(data) { $scope.contact = data; });
     $scope.selected_type = "unclass";
-    $scope.contact = {name: 'Brian Hulette', phones: {unclass: '(123)456-7890', mobile: '(789)123-4567'}};
-    $scope.selectType = function(new_type){
-      $scope.selected_type = new_type;
-    };
+    $scope.contact = {name: 'Brian Hulette', phones: [{type: 'unclass', phone: '(123)456-7890'}, {type: 'mobile', phone: '(789)123-4567'}]};
   }]);
 
   app.filter('capitalize', function() {
@@ -67,4 +64,63 @@
       return input.substring(0,1).toUpperCase() + input.substring(1);
     };
   });
+
+  app.directive('objectInput', function(){
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: {object: '='},
+      templateUrl: '/directives/dict_input.html',
+      controller: 'ObjectInputController',
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModelController) {
+        // parser/formatter to convert between model and view format
+        // model format: [{type: unclass, phone: 123}, {type: secure, phone: 456}]
+        // view format: {keys: [unclass, secure], data: {unclass: 123, secure: 456}}
+        ngModelController.$parsers.push(function(data) {
+          //convert data from view format to model format
+          model_data = [];
+          angular.forEach(data.keys, function(key) {
+            model_data.push({type: key, phone: data.data[key]});
+          });
+          return model_data;
+        });
+
+        ngModelController.$formatters.push(function(data) {
+          //convert data from model format to view format
+          view_data = {keys: [], data: {}};
+          angular.forEach(data, function(value) {
+            view_data.keys.push(value.type);
+            view_data.data[value.type] = value.phone;
+          });
+          return view_data;
+        });
+        
+        // Whenever viewValue changes, update our scope
+        ngModelController.$render = function() {
+          scope.keys = ngModelController.$viewValue.keys;
+          scope.data = ngModelController.$viewValue.data;
+        };
+        
+
+        // Make sure changing the keys and/or data actually changes the view
+        // (triggering a model change)
+        function updateView() {
+          ngModelController.$setViewValue({ keys: scope.keys, data: scope.data });
+        }
+        scope.$watch('keys', updateView, true);
+        scope.$watch('data', updateView, true);
+      }
+    };
+  });
+
+  app.controller('ObjectInputController', ['$scope', function($scope) {
+    $scope.status = {
+       open: false,
+       selected_key: ''
+       };
+    $scope.selectKey = function(new_type){
+      $scope.status.selected_key = new_type;
+    };
+  }]);
 })();
