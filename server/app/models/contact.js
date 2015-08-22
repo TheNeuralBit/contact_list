80c83101bedcb9a1f529a1073b78993e4927efca
Brian Hulette <hulettbh@gmail.com>
2015-08-22 15:24:12 -0700

    changed email in schema to emails

--- server/app/models/contact.js
+++ server/app/models/contact.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var ContactSchema = new mongoose.Schema({
   name:          {type: String, default: ''},
   phones:        [{type: {type: String}, phone: String}],
-  email:         [{type: {type: String}, email: String}],
+  emails:        [{type: {type: String}, email: String}],
   status:        {type: String, default: ''},
   out_of_office: {type: Boolean, default: false}
 });
