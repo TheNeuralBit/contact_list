1932079dc7cb5a225909fa3c03231ebadbcd4118
Brian Hulette <hulettbh@gmail.com>
2015-08-22 09:33:24 -0700

    created reusable ConvertUtil service for converting dictionary formats

--- public/js/app.js
+++ public/js/app.js
@@ -18,42 +18,68 @@
   
   app.factory('Contacts', ['$http', function($http) {
     result = {};
-    result.get = function(id) {
+    result.get = function get(id) {
       if (id === undefined)
         return $http.get('./api/contacts');
       else
         return $http.get('./api/contacts/' + id);
     };
 
-    result.create = function(contact) {
+    result.create = function create(contact) {
       return $http.post('./api/contacts/', contact);
     };
 
-    result.update = function(id, contact) {
+    result.update = function update(id, contact) {
       console.debug("Sending update:");
+      console.debug(id);
       console.debug(contact);
       return $http.put('./api/contacts/' + id, contact);
     };
 
-    result.delete = function(id) {
+    result.delete = function delete_(id) {
       return $http.delete('./api/contacts/' + id);
     };
 
     return result;
   }]);
 
-  app.controller('ContactListController', ['$scope', 'Contacts', function($scope, Contacts){
-    console.log('Creating ContactListController');
+  app.factory('ConvertUtil', function(){
+    result = {};
+
+    result.list_to_object = function list_to_object(lst) {
+      var obj = {keys: [], data: {}};
+      angular.forEach(lst, function(value) {
+        obj.keys.push(value.type);
+        obj.data[value.type] = value.phone;
+      });
+      return obj;
+    };
+
+    result.object_to_list = function object_to_list(obj) {
+      var lst = [];
+      angular.forEach(obj.keys, function(key) {
+        lst.push({type: key, phone: obj.data[key]});
+      });
+      return lst;
+    };
+
+    return result;
+  });
+
+  app.controller('ContactListController', ['$scope', 'Contacts', 'ConvertUtil', function($scope, Contacts, ConvertUtil){
     $scope.contacts_api = Contacts;
-    Contacts.get().success(function(data) { $scope.contacts = data; });
+    Contacts.get().success(function(data) { 
+      $scope.contacts = [];
+      for (var idx = 0; idx < data.length; idx++) {
+        data[idx].phones = ConvertUtil.list_to_object(data[idx].phones);
+        $scope.contacts.push(data[idx]);
+      }
+    });
   }]);
 
   app.controller('ContactController', ['$scope', '$routeParams', 'Contacts', function($scope, $routeParams, Contacts){
-    console.log('Creating ContactController');
-    $scope.contacts_api = Contacts;
-    $scope.contact_id = $routeParams.contact_id;
-    Contacts.get($scope.contact_id).success(function(data) { 
-      data = {name: 'Brian Hulette', phones: [{type: 'unclass', phone: '(123)456-7890'}, {type: 'mobile', phone: '(789)123-4567'}]};
+    var contact_id = $routeParams.contact_id;
+    Contacts.get(contact_id).success(function(data) { 
       $scope.contact = data; 
       $scope.original_contact = angular.copy(data);
     });
@@ -61,6 +87,11 @@
     $scope.$watch('contact', function() {
       $scope.clean =  angular.equals($scope.contact, $scope.original_contact); 
     }, true);
+    $scope.submit = function submit() {
+      Contacts.update(contact_id, $scope.contact).success(function(data) {
+        $scope.original_contact = data;
+      });
+    };
   }]);
 
   app.filter('capitalize', function() {
@@ -71,7 +102,7 @@
     };
   });
 
-  app.directive('objectInput', function(){
+  app.directive('objectInput', ['ConvertUtil', function(ConvertUtil){
     return {
       restrict: 'E',
       replace: true,
@@ -85,21 +116,14 @@
         // view format: {keys: [unclass, secure], data: {unclass: 123, secure: 456}}
         ngModelController.$parsers.push(function(data) {
           //convert data from view format to model format
-          model_data = [];
-          angular.forEach(data.keys, function(key) {
-            model_data.push({type: key, phone: data.data[key]});
-          });
-          return model_data;
+          var rtrn = ConvertUtil.object_to_list(data);
+          return rtrn;
         });
 
         ngModelController.$formatters.push(function(data) {
           //convert data from model format to view format
-          view_data = {keys: [], data: {}};
-          angular.forEach(data, function(value) {
-            view_data.keys.push(value.type);
-            view_data.data[value.type] = value.phone;
-          });
-          return view_data;
+          var rtrn = ConvertUtil.list_to_object(data);
+          return rtrn;
         });
         
         // Whenever viewValue changes, update our scope
