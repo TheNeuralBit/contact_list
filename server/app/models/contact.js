b201ec5552eb912b23fba4ca7dc3032cc852d3d0
Brian Hulette <hulettbh@gmail.com>
2015-08-22 09:28:37 -0700

    fixed bug with model since I used the name 'type'

--- server/app/models/contact.js
+++ server/app/models/contact.js
@@ -1,10 +1,10 @@
 var mongoose = require('mongoose');
 var ContactSchema = new mongoose.Schema({
-  name: {type: String, default: ''},
-  phones: [{type: String, number: String}],
-  email: [{type: String, email: String}],
-  out_of_office: {type: Boolean, default: false},
-  status: {type: String, default: ''}
+  name:          {type: String, default: ''},
+  phones:        [{type: {type: String}, phone: String}],
+  email:         [{type: {type: String}, email: String}],
+  status:        {type: String, default: ''},
+  out_of_office: {type: Boolean, default: false}
 });
 
 module.exports = mongoose.model('Contact', ContactSchema);
