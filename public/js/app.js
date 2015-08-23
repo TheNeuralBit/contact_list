4afd1f910fcfa044f66d709e41c78917a2ac4daa
Brian Hulette <hulettbh@gmail.com>
2015-08-23 08:16:04 -0700

    added add/delete contact buttons

--- public/js/app.js
+++ public/js/app.js
@@ -81,16 +81,41 @@
     return result;
   });
 
-  app.controller('ContactListController', ['$scope', 'Contacts', 'ConvertUtil', function($scope, Contacts, ConvertUtil){
+  app.controller('ContactListController', ['$scope', 'Contacts', 'ConvertUtil', '$location', function($scope, Contacts, ConvertUtil, $location){
     $scope.contacts_api = Contacts;
-    Contacts.get().success(function(data) { 
-      $scope.contacts = [];
-      for (var idx = 0; idx < data.length; idx++) {
-        data[idx].phones = ConvertUtil.list_to_object(data[idx].phones, 'type', 'phone');
-        data[idx].emails = ConvertUtil.list_to_object(data[idx].emails, 'type', 'email');
-        $scope.contacts.push(data[idx]);
+    $scope.refresh_contacts = function(data) {
+      Contacts.get().success(function(data) { 
+        $scope.contacts = [];
+        for (var idx = 0; idx < data.length; idx++) {
+          data[idx].phones = ConvertUtil.list_to_object(data[idx].phones, 'type', 'phone');
+          data[idx].emails = ConvertUtil.list_to_object(data[idx].emails, 'type', 'email');
+          $scope.contacts.push(data[idx]);
+        }
+      });
+    };
+    $scope.refresh_contacts();
+
+    $scope.header = "All Contacts";
+    $scope.search_value = '';
+    $scope.$watch('search_value', function() {
+      if ($scope.search_value === "") {
+        $scope.header = "All Contacts";
+      } else {
+        $scope.header = "Contacts Matching '" + $scope.search_value + "'";
       }
     });
+    $scope.deleteContact = function deleteContact(id) {
+      console.log('Deleting ' + id);
+      Contacts.delete(id);
+      $scope.refresh_contacts();
+    };
+    $scope.createNew = function createNew() {
+      Contacts.create({}).success(function(contact) {
+        console.log('Created ' + contact._id + '!');
+        var path = '/contact/' + contact._id;
+        $location.path(path);
+      });
+    };
   }]);
 
   app.controller('ContactController', ['$scope', '$routeParams', 'Contacts', function($scope, $routeParams, Contacts){
